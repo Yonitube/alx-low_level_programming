82d9f2f2e1fcf6631664b1deb723b94affa7c45f
Yonitube <bashaasefa@gmail.com>
2022-09-15 08:14:31 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
-#include “main.h”
+#include "main.h"
+
 
 /**
-* print_alphabet – function to print abc
+* print_alphabet - function to print abc
 *
 * Return: 0
 */
 void print_alphabet(void)
 {
-char c;
+int c;
 
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-_putchar(‘\n’);
+_putchar('\n');
 }
