8ee1f5b028446c7322b2b274529db6f88cf558ad
Yonitube <bashaasefa@gmail.com>
2022-09-27 08:50:07 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,14 +3,16 @@
 *main - prints out first 50
 *fibonacci suit numbers
 *Return: return 0
-*/                                                                     int main(void)
+*/
+int main(void)
 {
 int inc;
 unsigned long n1 = 0, n2 = 1, n3;
 for (inc = 0; inc < 50; inc++)
 {
 n3 = n1 + n2;
-printf("%lu", n3);                                                     n1 = n2;
+printf("%lu", n3);
+n1 = n2;
 n2 = n3;
 
 if (inc == 49)
